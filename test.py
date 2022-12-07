fde827dd84ac70745aa9cb140511686fad561c40
Tina <47900457+ShimaSama@users.noreply.github.com>
2022-12-07 17:11:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -34,6 +34,14 @@ def from_string(s):
 #process = os.popen('')
 #preprocessed = process.read()
 #process.close()
+total = 0
+false_positive = 0
+false_negative = 0
+true_positive = 0
+true_negative = 0
+good = 0
+bad = 0
+
 file = sys.argv[1]
 
 dataset = pandas.read_csv(file)
@@ -58,11 +66,31 @@ for elem in X:
 filename = 'finalized_RFC_model.sav'
 loaded_model = joblib.load(filename)
 for i in range (0,len(X)):
-    is_netflix = loaded_model.predict([X[i]])
-    if(is_netflix[0] == Y[i]):
-        print("good")
-    else:
-        print("bad")
-print(is_netflix)
+        total += 1
+        is_netflix = loaded_model.predict([X[i]])
+        if(is_netflix[0] == Y[i]):
+                print("good")
+                good += 1
+                if(is_netflix[0] == "no_netflix"):
+                        true_negative += 1
+                else:
+                        true_positive += 1
+        else:
+                print("bad")  
+                bad += 1  
+                if(is_netflix[0] == "no_netflix"): #label es no_netflix
+                        false_negative += 1
+                else:#label es netflix
+                        false_positive += 1
+
+#print(is_netflix)
+print("true positive = " + str(true_positive)+ "---> rate = " + str(true_positive/(true_positive+false_negative)))
+print("true negative = " + str(true_negative) + "---> rate = " + str(true_negative/(true_negative+false_positive)))
+print("false negative = " + str(false_negative) + "---> rate = " + str(false_negative/(false_negative+true_positive)))
+print("false positive = " + str(false_positive) + "---> rate = " + str(false_positive/(false_positive+true_negative)))
+print("sensitivity = " + str(true_positive/(true_positive+false_negative)))
+print("specifity = " + str(true_negative/(true_negative+false_positive)))
+print("acurracy = " + str(good/total))
+
 
 
